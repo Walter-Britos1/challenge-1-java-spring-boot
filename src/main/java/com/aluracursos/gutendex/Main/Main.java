ff1c9072a8cfc466f9508267e2430755b2d04461
Walter-Britos1 <walterbritos418@gmail.com>
2024-12-11 00:53:04 -0300

    adding search by name of books

--- src/main/java/com/aluracursos/gutendex/Main/Main.java
+++ src/main/java/com/aluracursos/gutendex/Main/Main.java
@@ -6,6 +6,8 @@ import com.aluracursos.gutendex.services.ApiClient;
 import com.aluracursos.gutendex.services.DataConverter;
 
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class Main {
     private static final String URL_API = "https://gutendex.com/books/";
@@ -13,6 +15,8 @@ public class Main {
     private ApiClient apiClient = new ApiClient();
 
     private DataConverter converter = new DataConverter();
+
+    private Scanner imput = new Scanner(System.in);
 
 
     public void showMenu() {
@@ -30,6 +34,26 @@ public class Main {
                 .limit(10)
                 .map(b -> b.title().toUpperCase())
                 .forEach(System.out::println);
+
+
+        // Busqueda de libro por nombre
+        System.out.println("¿Que libro desea leer hoy?");
+        var searchedBook = imput.nextLine();
+        json = apiClient.getData(
+                URL_API + "?search=" + searchedBook.replace(" ", "+").toUpperCase()
+        );
+
+        var foundBook = converter.getData(json, Data.class);
+        Optional<DataBooks> book = foundBook.booksList().stream()
+                .filter(b -> b.title().contains(searchedBook))
+                .findFirst();
+
+        if (book.isPresent()) {
+            System.out.println("El lobro que desea leer es: ");
+            System.out.println(book.get());
+        } else {
+            System.out.println("El libro no se pudo encontrar, intente nuevamente");
+        }
 
     }
 }
